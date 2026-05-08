9681f386d47aa108e3beaafd0a899f53b5533418
itchynisan <edmondchen5@gmail.com>
2026-05-08 13:18:49 +1000

    implementation of publish smart contract

---
+++ AO2.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {ERC721URIStorage, ERC721} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract Publish is ERC721URIStorage {
+
+    uint256 private _tokenID;
+
+    //functions can be called on fic and ARC is the symbol name
+    constructor() ERC721("Fic", "ARC") {}
+
+function mint(address author, string memory uri) public returns (uint256) {
+    uint256 token = _tokenID++;
+    _mint(author, token);
+    _setTokenURI (token, uri);
+    
+    return token;
+}
+
+}
+contract Sales {
+mapping(address => bool) public HighTrust;
+
+modifier onlyHighTrust() {
+    require(HighTrust[msg.sender], "Only a publisher or an archivist can execute this");
+    _;
+
+}
+}
